af1b8cf238e37e4f1dc27cc13a3cbad576ba7aec
heisson <heisson70@gmail.com>
2020-06-11 23:37:16 -0300

    More closures

---
+++ lecture1/0-closureBug.js
@@ -0,0 +1,15 @@
+function makeFunctionArray() {
+    const arr = []
+
+    for (var i = 0; i < 5; i++) {
+        arr.push(function () {
+            console.log(i)
+        })
+    }
+    // console.log(i);
+    return arr
+}
+
+const functionArr =  makeFunctionArray()
+
+functionArr[0]()
