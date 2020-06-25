23490fe971eee7db4adc518a05f2cd8cb51bf29d
heisson <heisson70@gmail.com>
2020-06-25 11:14:47 -0300

    Stack navigation finally working

---
+++ projects/project2/screens/MovieInfoScreen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+
+export default class MovieInfoScreen extends React.Component {
+  render () {
+    return (
+      <View>
+        <Text>
+          Movie info screen
+        </Text>
+      </View>
+    )
+  }
+}
