f03dee74ea45a937f828e04ee182aed982ddd6a0
heisson <heisson70@gmail.com>
2020-06-25 10:36:02 -0300

    Added stack navigation library

--- projects/project2/App.js
+++ projects/project2/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Constants from 'expo-constants';
+// import { createStackNavigator } from 'react-navigation-stack';
 
+
+// const MainStack = createStackNavigator({
+//
+// })
 
 export default function App() {
   return (
